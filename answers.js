bf0fece08f7e70e6718fbf7d46bab55b05576a18
Nadra R <nadra@Nadras-MacBook-Pro.local>
2023-07-27 14:13:16 -0400

    Fizz Buzz answered

--- answers.js
+++ answers.js
@@ -7,4 +7,17 @@ for (let i = 1; i <= 20; i++) {
 for (let i = 0; i <= 200; i += 2) {
     console.log(i);
   }
+  //Fizz Buzz
+  for (let i = 1; i <= 100; i++) {
+    let output = "";
   
+    if (i % 3 === 0) {
+      output += "Fizz";
+    }
+  
+    if (i % 5 === 0) {
+      output += "Buzz";
+    }
+  
+    console.log(output || i);
+  }
